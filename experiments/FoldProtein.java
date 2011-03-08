d8137cb159b86a0fdc85271643510e02b75dd810
= <=>
2011-03-08 22:58:37 +0100

    Updated the experiment to use the AdjustableChainTree class.

--- experiments/FoldProtein.java
+++ experiments/FoldProtein.java
@@ -9,6 +9,7 @@ import j3dScene.J3DScene;
 import geom3d.PointSet3d;
 import geom3d.Shape3d;
 import chainTree.ChainTree;
+import chainTree.AdjustableChainTree;
 import molecule.Protein;
 
 /**
@@ -32,101 +33,26 @@ public class FoldProtein {
 		Protein protein = new Protein(pdbId, 2, true);
 			
 		// setup protein to simulate
-		ChainTree cTree = setupChainTree(protein);
+		J3DScene scene = J3DScene.createJ3DSceneInFrame();
+		scene.setWindowTitle(pdbId + " - simulation");
+		
+		AdjustableChainTree cTree = new AdjustableChainTree(pdbId, scene);
 
-		cTree.j3dg.autoZoom();
-		cTree.j3dg.setWindowTitle(pdbId + " - simulation");
 		
 		// copy target into the scene of the chain tree
-		ChainTree target = setupChainTree(protein);
-		target.j3dg.frame.dispose();
+		ChainTree target = new AdjustableChainTree(pdbId, scene);
 
-		Map<Shape3d,Color> targetShapes = target.j3dg.getAllShapes();
-		for (Entry<Shape3d, Color> value : targetShapes.entrySet()) {
-			Shape3d shape = value.getKey();
-			Color color = value.getValue();
-
-			cTree.j3dg.addShape(shape, new Color(color.getRed(), color.getGreen(), color.getBlue(), targetOpacity));
-		}
-		
 		// rum simulation
-		obfuscateProtein(cTree);
+		cTree.unfold();
 		foldProtein(cTree, errorTolerance);
 	}
 
-	
-	/**
-	 * Sets up a chain tree for the given protein.
-	 * @param protein
-	 * @return
-	 */
-	private static ChainTree setupChainTree(Protein protein) {
-		/*
-		 * Create chain tree.
-		 */
-		PointSet3d allPoints = protein.getPointSet();
-		PointSet3d points = new PointSet3d();
-		for (int i = 0; i < allPoints.getSize(); i++) points.insert(allPoints.get(i));
-
-		ChainTree cTree = new ChainTree(points, protein, true);
-		
-		/*
-		 * Lock peptide planes and chain endpoints.
-		 */
-		for (int i = 2; i < cTree.nodes.length; i = i+3) cTree.nodes[i].isLocked = true;
-		cTree.nodes[cTree.nodes.length-1].isLocked = true;
-		cTree.nodes[0].isLocked = true;
-		
-		/*
-		 * Lock secondary structures.
-		 */
-		cTree.lockAlphaHelices(true);
-		cTree.lockBetaStrands(true);
-		
-		cTree.removeLockedSubtrees(true);
-		cTree.newRebalanceSubtree(cTree.root);
-		cTree.addLockedSubtrees(true);
-		
-		/*
-		 * Compute three bounding volume and energy.
-		 */
-		cTree.createBoundingVolume(cTree.root);
-		cTree.createEnergyBoundingVolume(cTree.root);
-
-		/*
-		 * Pre-compute distance matrix. <-- silly but avoids error
-		 */
-		cTree.getDistanceMatrix();
-		
-		/*
-		 * Setup 3D display properties.
-		 */
-		cTree.j3dg = J3DScene.createJ3DSceneInFrame();
-		cTree.initPaint();
-
-		return cTree;
-	}
-	
-	/**
-	 * Obfuscates a protein (stretches it).
-	 * @param cTree
-	 * @return
-	 */
-	private static void obfuscateProtein(ChainTree cTree) {
-		for (int i = 1; i < cTree.nodes.length-1; i++) {
-			if (!cTree.nodes[i].isLocked) { 
-				// if bound is not locked then obfuscate
-				cTree.changeRotationAngle(i, -cTree.getDihedralAngle(i));
-			}
-		}
-	}
-	
 	/**
 	 * Performs the folding of a protein.
 	 * @param cTree
 	 * @return
 	 */
-	private static void foldProtein(ChainTree cTree, double errorTolerance) {
+	private static void foldProtein(AdjustableChainTree cTree, double errorTolerance) {
 		double rmsd = cTree.getPairwiseDistanceRMSD(); // the root mean square distance from the actual protein structure.
 		double rmsdUpperBound = rmsd; // upper bound
 		int iterationsSinceLastImprovement = 0; // the number of iterations since the last improvement of the protein structure
@@ -143,8 +69,7 @@ public class FoldProtein {
 
 		
 		// Detect rotatable bonds.
-		ArrayList<Integer> rotatableBonds = new ArrayList<Integer>();
-		for (int i=0; i < backboneLength; i++) if (!cTree.nodes[i].isLocked) rotatableBonds.add(i);
+		ArrayList<Integer> rotatableBonds = new ArrayList<Integer>(cTree.rotateableBonds());
 		int rotatableBondsLength = rotatableBonds.size();
 		
 		/*
